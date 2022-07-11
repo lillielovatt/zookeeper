3e0efac900f17cbf4a4746e92a66b586af0a56e5
lillielove <lillielove@gmail.com>
2022-07-11 16:06:11 -0700

    successful post command, data validation, require fs and path modules so we can write client POST request data to server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path"); // provides utilities for working with file and directory paths
 const { animals } = require("./data/animals.json");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -46,6 +53,20 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    // function's main code HERE
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, "./data/animals.json"),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    // return finished code to post route for response
+    return animal;
+}
+
 app.get("/api/animals", (req, res) => {
     let results = animals;
     if (req.query) {
@@ -56,9 +77,9 @@ app.get("/api/animals", (req, res) => {
 
 app.get("/api/animals/:id", (req, res) => {
     const result = findById(req.params.id, animals);
-    if(result){
+    if (result) {
         res.json(result);
-    } else{
+    } else {
         res.send(404);
     }
 });
@@ -67,6 +88,46 @@ app.get("/api/animals/:id", (req, res) => {
 
 //send() wih the res (response parameter) to send "Hello!" to client
 
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== "string") {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== "string") {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== "string") {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
+
+app.post("/api/animals", (req, res) => {
+    // set id based on what the next index of the array will be (works only because we do not intend to remove data in this mod)
+    req.body.id = animals.length.toString();
+    // add animal to json file and animals array in this function
+
+    if (!validateAnimal(req.body)) {
+        res.status(400).send("The animal is not properly formatted.");
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(req.body);
+    }
+    // req.body is where our incoming content will be
+
+    // In order for our server to accept incoming data the way we need it to, 
+    // we need to tell our Express.js app to intercept our POST request before it gets to the callback function. 
+    // At that point, the data will be run through a couple of functions 
+    // to take the raw data transferred over HTTP and convert it to a JSON object.
+});
+
+
+
+
+
+// Even though adding app.listen() at the end is common practice, it is not required because it is just another method on app.
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
