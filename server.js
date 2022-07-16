fc34eea21c29a117b082fcf05418171c09ffcf81
lillielove <lillielove@gmail.com>
2022-07-16 09:26:41 -0700

    added get routes to all js files in public folder, for animals and zookeeper. Also added post route and functionality to script.js

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static("public"));
+// The way it works is that we provide a file path to a location in our application 
+// (in this case, the public folder) and instruct the server to make these files static resources.
+//  This means that all of our front-end code can now be accessed without having a specific server endpoint created for it!
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -32,9 +36,6 @@ function filterByQuery(query, animalsArray) {
         });
     }
 
-
-
-
     if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
@@ -53,20 +54,19 @@ function findById(id, animalsArray) {
     return result;
 }
 
-function createNewAnimal(body, animalsArray) {
-    // function's main code HERE
-    const animal = body;
-    animalsArray.push(animal);
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
 
-    fs.writeFileSync(
-        path.join(__dirname, "./data/animals.json"),
-        JSON.stringify({ animals: animalsArray }, null, 2)
-    );
+app.get('/animals', (req,res)=>{
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
 
-    // return finished code to post route for response
-    return animal;
-}
+app.get('/zookeepers',(req,res)=>{
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
 
+//can't have 2 gets with the same ROUTE
 app.get("/api/animals", (req, res) => {
     let results = animals;
     if (req.query) {
@@ -83,10 +83,33 @@ app.get("/api/animals/:id", (req, res) => {
         res.send(404);
     }
 });
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 //get requires 2 args - string that describes the route the client will have to fetch from,
 // and CB function that executes every time route is accessed with GET request
 
 //send() wih the res (response parameter) to send "Hello!" to client
+
+app.post("/api/animals", (req, res) => {
+    // set id based on what the next index of the array will be (works only because we do not intend to remove data in this mod)
+    req.body.id = animals.length.toString();
+    // add animal to json file and animals array in this function
+
+    if (!validateAnimal(req.body)) {
+        res.status(400).send("The animal is not properly formatted.");
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+    // req.body is where our incoming content will be
+
+    // In order for our server to accept incoming data the way we need it to, 
+    // we need to tell our Express.js app to intercept our POST request before it gets to the callback function. 
+    // At that point, the data will be run through a couple of functions 
+    // to take the raw data transferred over HTTP and convert it to a JSON object.
+});
 
 function validateAnimal(animal) {
     if (!animal.name || typeof animal.name !== "string") {
@@ -104,33 +127,34 @@ function validateAnimal(animal) {
     return true;
 }
 
-app.post("/api/animals", (req, res) => {
-    // set id based on what the next index of the array will be (works only because we do not intend to remove data in this mod)
-    req.body.id = animals.length.toString();
-    // add animal to json file and animals array in this function
+function createNewAnimal(body, animalsArray) {
+    // function's main code HERE
+    const animal = body;
+    animalsArray.push(animal);
 
-    if (!validateAnimal(req.body)) {
-        res.status(400).send("The animal is not properly formatted.");
-    } else {
-        const animal = createNewAnimal(req.body, animals);
-        res.json(req.body);
-    }
-    // req.body is where our incoming content will be
+    fs.writeFileSync(
+        path.join(__dirname, "./data/animals.json"),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
 
-    // In order for our server to accept incoming data the way we need it to, 
-    // we need to tell our Express.js app to intercept our POST request before it gets to the callback function. 
-    // At that point, the data will be run through a couple of functions 
-    // to take the raw data transferred over HTTP and convert it to a JSON object.
-});
-
-
-
-
+    // return finished code to post route for response
+    return animal;
+}
 
 // Even though adding app.listen() at the end is common practice, it is not required because it is just another method on app.
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
-
 // http://localhost:3001/api/animals
+//https://evening-headland-92865.herokuapp.com/api/animals
+
+// {
+// 	"name": "Larry",
+//   "species": "Lemur",
+//   "diet": "omnivore",
+//   "personalityTraits": ["hungry"]
+// }
+
+// we need to make sure that every time we test something on the front end 
+// that it is served from http://localhost:3001, or https://<your-heroku-app-name>.herokuapp.com when it's in production.
